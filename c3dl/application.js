a1982b0a412eaa919722aed048d31754bbe6ab36
juliusf <julius.flohr@gmail.com>
2014-01-03 00:45:33 +0100

    new changes

--- c3dl/application.js
+++ c3dl/application.js
@@ -5,9 +5,111 @@
 // main. The argument being passed - "tutorial" -
 // is the id of the canvas element on the html page.
 
-c3dl.addMainCallBack(canvasMain, "ship");
-c3dl.addModel("Apple.dae");
-var ship;
+c3dl.addMainCallBack(canvasMain, "pool");
+c3dl.addModel("queue.dae")
+c3dl.addModel("table.dae")
+var pool;
+var table;
+var queue;
+var isDragging = false; //tracks or not the user is currently dragging the mouse
+var rotationStartCoords = [0,0]; //The mouse coordinates at the beginning of a rotation
+var SENSITIVITY = 0.7;
+
+//Called when the user releases the left mouse button.
+//Records that the user is no longer dragging the mouse
+function mouseUp(evt)
+{
+	if(evt.which == 1)
+	{
+		isDragging = false;
+	}
+}
+
+//Called when the user presses the left mouse button.
+//Records that the user may start to drag the mouse, along with the current X & Y
+// coordinates of the mouse.
+function mouseDown(evt)
+{
+	if(evt.which == 1)
+	{
+		isDragging = true;
+		rotationStartCoords[0] = xevtpos(evt);
+		rotationStartCoords[1] = yevtpos(evt);
+	}
+}
+
+//Called when the mouse moves
+//This function will only do anything when the user is currently holding
+// the left mouse button.  It will determine how far the cursor has moved
+// since the last update and will pitch and yaw the camera based on that
+// amount and the sensitivity variable.
+function mouseMove(evt)
+{
+	if(isDragging == true)
+	{
+        var cam = scn.getCamera();
+		var x = xevtpos(evt);
+		var y = yevtpos(evt);
+		
+		// how much was the cursor moved compared to last time
+		// this function was called?
+		var deltaX = x - rotationStartCoords[0];
+                var deltaY = y - rotationStartCoords[1];
+
+		cam.yaw(-deltaX * SENSITIVITY);
+		cam.pitch(deltaY * SENSITIVITY);
+		
+		// now that the camera was updated, reset where the
+		// rotation will start for the next time this function is 
+		// called.
+		rotationStartCoords = [x,y];
+	}
+}
+
+//Calculates the current X coordinate of the mouse in the client window
+function xevtpos(evt)
+{
+    return 2 * (evt.clientX / evt.target.width) - 1;
+}
+
+//Calculates the current Y coordinate of the mouse in the client window
+function yevtpos(evt)
+{
+    return 2 * (evt.clientY / evt.target.height) - 1;
+}
+
+//This function is called when the user moves the scroll-wheel on the mouse
+//Depending on the direction of the action, the camera will either get
+// closer to or farther from the orbit point.
+function mouseScroll(evt) {
+	var cam = scn.getCamera();
+	if(-evt.detail*ZOOM_SENSITIVITY < 0)
+		{
+			cam.goFarther(-1 * -evt.detail*ZOOM_SENSITIVITY);
+		}
+		
+		// towards screen
+		else
+		{
+			cam.goCloser(-evt.detail*ZOOM_SENSITIVITY);
+		}
+}
+
+//This function is called whenever a key is released.
+//If the key released was the space-bar, the camera will switch
+// which object it is orbiting.
+function swapOrbitPoint(evt) {
+	if(evt.keyCode == 32) {
+		var cam = scn.getCamera();
+		//compare the camera's orbit point with the location of the duck
+		if(c3dl.isVectorEqual(cam.getOrbitPoint(),duck.getPosition())) {
+			cam.setOrbitPoint(teapot.getPosition());
+		}
+		else{
+			cam.setOrbitPoint(duck.getPosition());
+		}
+	}
+}
 
 // The program main
 function canvasMain(canvasName){
@@ -29,43 +131,54 @@ function canvasMain(canvasName){
  //initialize this will return false but only after you
  //try to attach it to a scene.
  if(renderer.isReady() )
- {
- // Create a Collada object that
- // will contain a imported
- // model of something to put
- // in the scene.
- ship = new c3dl.Collada();
+ {   
 
- // If the path is already parsed
- // (as it is in this case)
- // then the model is automatically retrieved
- // from a collada manager.
- ship.init("Apple.dae");
-ship.setTexture("stone.png");
+ pool = new c3dl.Sphere(5);
+ table = new c3dl.Plane(500,500)
+ queue = new c3dl.Collada();
+
+queue.init("queue.dae")
+pool.setTexture("billard_black_8.png");
+pool.setPosition(new Array(0.0,2.0,0.0))
+table.setTexture("felt.png")
  // Give the duck a bit of a spin on y
- ship.setAngularVel(new Array(0.0, -0.001, 0.00));
+ 
 
  // Add the object to the scene
- scn.addObjectToScene(ship);
+ scn.addObjectToScene(pool);
+ scn.addObjectToScene(table);
+ scn.addObjectToScene(queue);
 
  // Create a camera
- var cam = new c3dl.FreeCamera();
+  var cam = new c3dl.OrbitCamera();
+  cam.setFarthestDistance(1000);
+  cam.setClosestDistance(20);	
+  cam.setOrbitPoint(pool.getPosition());
+  cam.setDistance(150);
+  scn.setCamera(cam);
 
- // Place the camera.
- // WebGL uses a right handed co-ordinate system.
- // move 200 to the right
- // move 300 up
- // move 500 units out
- cam.setPosition(new Array(10.0, 10.0, 10.0));
 
- // Point the camera.
- // Here it is pointed at the same location as
- // the duck so the duck will appear centered.
- cam.setLookAtPoint(new Array(0.0, 0.0, 0.0));
+// Add some lights
+ var spot = new c3dl.SpotLight();
+ spot.setPosition(new Array(0.0, 150.0, 0.0));
+ spot.setDirection(new Array(0.0, -1.0,0.0));
+ spot.setSpecular([0.5,0.5,0.5,1.0]);
+ spot.setDiffuse([0.2,0.2,0.2,1.0]);
+ spot.setAmbient([0.2,0.2,0.2,1.0]);
+ spot.setCutoff(180);
+ spot.setExponent(1)
+ spot.setOn(true);
 
- // Add the camera to the scene
- scn.setCamera(cam);
+ scn.addLight(spot);
 
+ /*var spec = new c3dl.DirectionalLight();
+ spec.setName('spec');
+ spec.setDirection([0.0,0.0,0.0]);
+ spec.setSpecular([0.3,0.3,0.3,0.6])
+ spec.setOn(true);
+ scn.addLight(spec); */
+  scn.setAmbientLight([0,0,0,0]);
+ scn.setMouseCallback(mouseUp,mouseDown, mouseMove);
  // Start the scene
  scn.startScene();
  }
